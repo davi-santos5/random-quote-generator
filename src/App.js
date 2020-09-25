b474eb331aca777023ddf3637f6bc822b01a1a49
davi-santos5 <dbs.tj0@gmail.com>
2020-09-25 19:39:38 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import './App.css'
 
 function App() {
+  const [{ 
+    quote,
+    author,
+    idx
+  }, setState] = useState({
+    quote: ['Melhor faltar do que não ter', 'Mais chato do que chinelo de gordo', 'Mudou o sacerdote, mudou a lei','Toca a saca do faqueiro', 'Random quote here','Another quote','Hello There'],
+    author: ['Berinja', 'Bozos', 'Lan Santos', 'Fitor', 'Random Author','Somebody','General Kenobi'],
+    idx: 0
+  })
+
+
+  function changeQuote() {
+    var i = Math.floor(Math.random() * quote.length)
+    while(i === idx){
+      i = Math.floor(Math.random() * quote.length)
+    }
+    setState({
+     quote,
+     author,
+     idx: i
+    }) 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='quote-box'>
+      <h1 id='text'>{quote[idx]}</h1>
+      <p id='author'><span>- </span>{author[idx]}</p>
+      <button id='new-quote' onClick= {changeQuote}>New Quote</button>
+      <a href="twitter.com/intent/tweet" id='tweet-quote'><i class="fab fa-twitter-square fa-2x"></i></a>
     </div>
-  );
+  )
 }
 
 export default App;
